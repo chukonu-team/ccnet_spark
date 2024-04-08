18b3dfc5a126e00719c1de03823c9238b52f60a7
SteveWang63 <1321429886@qq.com>
2024-04-08 13:50:18 +0800

    a

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from ccnet_spark.pipeline import Pipeline,Config
 import time
 
 config=Config(isSample=True,n_segments=4,sampleRate=0.1,cache_dir="../cache_data/",dump="2019-18")
-
+print(config)
 pipeline=Pipeline(config)
 df=pipeline.load_data()
 s=time.time()
