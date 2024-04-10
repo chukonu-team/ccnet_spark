b09dd500ffaf744fae4e37feb76ac714b50de37a
SteveWang63 <1321429886@qq.com>
2024-04-10 17:52:13 +0800

    a

---
+++ profile.md
@@ -0,0 +1,15 @@
+# profile
+
+如何测试在集群单节点测试，使用1segment采用0.01采样率，共400左右doc，使用.rdd.count()，可以发现pipeline:['real_len', 'hash']反而比pipeline:['real_len', 'hash', 'dedup_keep']长，说明.rdd.count()本身耗时也不少。
+pipeline:[], time consume:1.847860336303711
+pipeline:['real_len'], time consume:1.9338457584381104
+pipeline:['real_len', 'hash'], time consume:13.863111019134521
+pipeline:['real_len', 'hash'], time consume:13.343939542770386
+pipeline:['real_len', 'hash', 'dedup_keep'], time consume:6.563436985015869
+pipeline:['real_len', 'hash', 'dedup_keep'], time consume:6.483687400817871
+pipeline:['real_len', 'hash', 'dedup_keep', 'lid'], time consume:6.867429494857788
+pipeline:['real_len', 'hash'], time consume:13.393240213394165
+pipeline:['real_len', 'hash', 'dedup_keep', 'lid', 'sp'], time consume:8.507328748703003
+pipeline:['real_len', 'hash', 'dedup_keep', 'lid', 'sp', 'lm'], time consume:9.783689737319946
+pipeline:['real_len', 'hash', 'dedup_keep', 'lid', 'sp', 'lm', 'pp_bucket'], time consume:10.074767351150513
+pipeline:['real_len', 'hash', 'dedup_keep', 'lid', 'sp', 'lm', 'pp_bucket', 'drop'], time consume:10.004109382629395
--- test2/run.py
+++ test2/run.py
@@ -1,16 +1,10 @@
 from ccnet_spark.pipe_line import Pipeline, Config
 import time
 from pyspark.sql import SparkSession
-spark = (
-    SparkSession.builder.appName("CCNETSpark_ONLY")
-    # .master("local[*]")
-    .config("spark.executor.memory", "100g")
-    .config("spark.driver.memory", "100g")
-    .config("spark.driver.maxResultSize", "100g")
-    .config("spark.sql.execution.arrow.pyspark.enabled", "true")
-    .getOrCreate()
-)
-pips = [
+import sys
+
+def getPIP(index):
+    pips = [
     [],
     [
         "real_len",
@@ -65,47 +59,45 @@ pips = [
         "drop",
     ],
 ]
+    return pips[index]
 
-# spark = (
-#     SparkSession.builder.appName("CCNETSpark_ONLY")
-#     .master("local[*]")
-#     .config("spark.executor.memory", "100g")
-#     .config("spark.driver.memory", "100g")
-#     .config("spark.driver.maxResultSize", "100g")
-#     .config("spark.sql.execution.arrow.pyspark.enabled", "true")
-#     # .config("spark.executor.extraJavaOptions", "-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps")
-#     # .config("spark.driver.extraJavaOptions", "-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps")
-#     .getOrCreate()
-# )
+spark = (
+    SparkSession.builder.appName("CCNETSpark_ONLY")
+    # .master("local[*]")
+    .config("spark.executor.memory", "100g")
+    .config("spark.driver.memory", "100g")
+    .config("spark.driver.maxResultSize", "100g")
+    .config("spark.sql.execution.arrow.pyspark.enabled", "true")
+    # .config("spark.executor.extraJavaOptions", "-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps")
+    # .config("spark.driver.extraJavaOptions", "-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps")
+    .getOrCreate()
+)
 
-times = []
-for index,p in enumerate(pips):
-    if(index!=6):
-        continue
+if __name__ == "__main__":
+    # 从命令行参数中获取索引
+    index = int(sys.argv[1])
+    pip=getPIP(index)
+    print(f"pipline is:{pip}")
     config = Config(
-        isSample=True,
-        n_segments=1,
-        sampleRate=0.01,
-        cache_dir="../../cached_data/",
-        output_dir="../../cached_data/",
-        fasttext_model_path='../../cc_net/bin/lid.bin', 
-        lm_dir='../../cc_net/data/lm_sp', 
-        cutoff_csv_path='../../cc_net/cc_net/data/cutoff.csv',
-        dump="2019-18",
-        pipeline=p,
-    )
-
+            isSample=True,
+            n_segments=1,
+            sampleRate=0.01,
+            cache_dir="../../cached_data/",
+            output_dir="../../cached_data/",
+            fasttext_model_path="../../cc_net/bin/lid.bin",
+            lm_dir="../../cc_net/data/lm_sp",
+            cutoff_csv_path="../../cc_net/cc_net/data/cutoff.csv",
+            dump="2019-18",
+            pipeline=pip,
+        )
+    
     pipeline = Pipeline(config,spark)
     df = pipeline.load_data()
     s = time.time()
     pipeline.run()
     res=pipeline.df.rdd.count()
     # pipeline.save_data()
-
     e = time.time()
-    times.append(e - s)
-    print(f"pipeline:{pips[index]}, time consume:{e-s}")
+    print("==============================================")
+    print(f"pipeline:{pip}, time consume:{e-s}")
 
-for index,t in enumerate(times):
-    print("=====================================")
-    print(f"pipeline:{pips[index]},comsume:{t}s")
