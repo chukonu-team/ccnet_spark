fe269fb1de71cec7ec89fa019c0f867f6eaa1f94
zizdlp <zdlp@sina.cn>
2024-05-15 14:59:28 +0800

    bugfix & remove union

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ build_base_ccnet:
 	sudo docker build -t base-service-ccnet .
 run_ccnet:
 	sudo docker run -itd --network host --name base-service-ccnet -v "$(shell pwd):/app" -v "/metadata0:/metadata0" base-service-ccnet
+clean:
+	sudo docker stop base-service-ccnet
+	sudo docker rm base-service-ccnet
 use_ccnet:
 	sudo docker exec -it base-service-ccnet bash
 install_ccnet:
--- ccnet_spark/pipe_save.py
+++ ccnet_spark/pipe_save.py
@@ -25,7 +25,7 @@ def save_tmp(
 
         os.makedirs("/".join(saved_sdf_path.split("/")[:-1]), exist_ok=True)
         spark_df.write.mode("overwrite").parquet(
-            f"{file}:///{saved_sdf_path}"
+            f"file:///{saved_sdf_path}"
         )
 def save_partation(
     spark_df,
@@ -50,7 +50,7 @@ def save_partation(
 
         os.makedirs("/".join(saved_sdf_path.split("/")[:-1]), exist_ok=True)
         spark_df.write.mode("overwrite").partitionBy("lang", "bucket").parquet(
-            f"{file}:///{saved_sdf_path}"
+            f"file:///{saved_sdf_path}"
         )
 def load_partation(
     spark,
@@ -71,13 +71,12 @@ def load_partation(
         saved_sdf_path = os.path.join(hdfs_dir,"hdfs_result_sdf_parquet",dump,saved_sdf_name)
         df = spark.read.parquet(f"{hdfs_hdfs_url}{saved_sdf_path}/lang={lang}/bucket={bucket}")
     else:
-        save_type = "hdfs" if use_hdfs else "file"
         saved_sdf_name = "_sampleRate_" + str(int(sampleRate*100 if isSample else 100)) + "_min_len_"+str(min_len)+".parquet"
-        saved_sdf_path = os.path.join(output_dir,"sdf_parquet",dump,saved_sdf_name)
+        saved_sdf_path = os.path.join(output_dir,"result_sdf_parquet",dump,saved_sdf_name)
         saved_sdf_path = convert_to_absolute_path(saved_sdf_path)
         if not os.path.exists(saved_sdf_path):
             os.makedirs("/".join(saved_sdf_path.split("/")[:-1]), exist_ok=True)
-        df = spark.read.parquet(f"{save_type}:///{saved_sdf_path}/lang={lang}/bucket={bucket}")
+        df = spark.read.parquet(f"file:///{saved_sdf_path}/lang={lang}/bucket={bucket}")
     return df
 def load_all(
     spark,
@@ -96,13 +95,12 @@ def load_all(
         saved_sdf_path = os.path.join(hdfs_dir,"hdfs_result_sdf_parquet",dump,saved_sdf_name)
         df = spark.read.parquet(f"{hdfs_hdfs_url}{saved_sdf_path}")
     else:
-        save_type = "hdfs" if use_hdfs else "file"
         saved_sdf_name = "_sampleRate_" + str(int(sampleRate*100 if isSample else 100)) + "_min_len_"+str(min_len)+".parquet"
-        saved_sdf_path = os.path.join(output_dir,"sdf_parquet",dump,saved_sdf_name)
+        saved_sdf_path = os.path.join(output_dir,"result_sdf_parquet",dump,saved_sdf_name)
         saved_sdf_path = convert_to_absolute_path(saved_sdf_path)
         if not os.path.exists(saved_sdf_path):
             os.makedirs("/".join(saved_sdf_path.split("/")[:-1]), exist_ok=True)
-        df = spark.read.parquet(f"{save_type}:///{saved_sdf_path}")
+        df = spark.read.parquet(f"file:///{saved_sdf_path}")
     return df
 def analy_df(df):
     # 定义聚合函数求和
